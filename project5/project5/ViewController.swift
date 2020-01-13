0038f18252c37aabd68fa4369fe670a53fa2c5df
Thế Anh Huỳnh <theanh.petech@gmail.com>
2020-01-13 21:18:53 +0700

    second step load data to tableviewcell is done

--- project5/project5/ViewController.swift
+++ project5/project5/ViewController.swift
@@ -32,6 +32,16 @@ class ViewController: UITableViewController {
         useWords.removeAll(keepingCapacity: true)
         tableView.reloadData()
     }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return allWords.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Word", for: indexPath)
+        cell.textLabel?.text = allWords[indexPath.row]
+        return cell
+    }
 
 }
 
