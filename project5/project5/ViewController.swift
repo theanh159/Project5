691219b56698682253f0591907f41d63db4f7cee
Thế Anh Huỳnh <theanh.petech@gmail.com>
2020-01-13 22:19:13 +0700

    third step build closures not yet done

--- project5/project5/ViewController.swift
+++ project5/project5/ViewController.swift
@@ -16,6 +16,8 @@ class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promtForAnswer))
+        
         if let startWordPath =  Bundle.main.path(forResource: "start", ofType: "txt") {
             if let startWord = try? String(contentsOfFile: startWordPath) {
                 allWords = startWord.components(separatedBy: "\n")
@@ -41,6 +43,44 @@ class ViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Word", for: indexPath)
         cell.textLabel?.text = allWords[indexPath.row]
         return cell
+    }
+    
+    @objc func promtForAnswer() {
+        let ac = UIAlertController(title: "Enter Answer", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        let submitAction = UIAlertAction(title: "Submit", style: .default) { [unowned self, ac] (action: UIAlertAction) in
+              let answer = ac.textFields![0]
+              self.submit(answer: answer.text!)
+           }
+        ac.addAction(submitAction)
+        present(ac, animated: true, completion: nil)
+    }
+    
+    func submit(answer: String) {
+        let lowerAnswer = answer.lowercased()
+        
+        if isPossible(word: lowerAnswer){
+            if isOriginal(word: lowerAnswer){
+                if isReal(word: lowerAnswer){
+                    useWords.insert(answer, at: 0)
+                    
+                    let indexPath = IndexPath(row: 0, section: 0)
+                    tableView.insertRows(at: [indexPath], with: .automatic)
+                }
+            }
+        }
+    }
+    
+    func isPossible(word: String) -> Bool {
+        return true
+    }
+    
+    func isOriginal(word: String) -> Bool {
+        return true
+    }
+    
+    func isReal(word: String) -> Bool {
+        return true
     }
 
 }
